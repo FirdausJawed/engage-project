3be53df2acf23361732a8fc97df0c29c9c54e0fe
Firdaus Jawed <85278666+FirdausJawed@users.noreply.github.com>
2022-05-29 16:17:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ if selected_option=="Movie":
                 except:
                     print("Api call failed")
 
-elif selected_option=="Ratings":
+elif selected_option=="Genre":
     st.header("Genre Based Recommendation System")
     genre = st.selectbox(
         "What's your favorite movie genre",
